aaa7ba12442b526b7ffb3e72a7f52a978708482b
Richard Batty <rjlbatty@gmail.com>
2014-12-28 20:17:33 +0000

    initial commit

---
+++ init.el
@@ -0,0 +1,20 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+)
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+)
+
+(defconst user-init-dir "~/.emacs.d/custom")
+
+(defun load-user-file (file)
+  (interactive "f")
+  (load-file (expand-file-name file user-init-dir)))
+
+(load-user-file "mouse.el")
